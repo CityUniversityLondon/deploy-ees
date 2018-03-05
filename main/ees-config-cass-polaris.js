15278881cd979a95ffb001f886b9f43b3760882e
CUL Web Team <ucs-webteam@city.ac.uk>
2018-03-05 12:59:29 +0000

    03/05/18 12:59:29 | polaris asset finder config

---
+++ main/ees-config-cass-polaris.js
@@ -0,0 +1,1 @@
+!function(o){o.EasyEditConfig.assetFinderLocations=[17029,17031,87,344333],o.EasyEditConfig.pluginConfig={relatedAssets:{metadataFields:[{name:"associated courses",id:19501,rootNode:951,focusNode:951},{name:"location",id:19938,rootNode:9508,focusNode:9508}]}}}(window);
